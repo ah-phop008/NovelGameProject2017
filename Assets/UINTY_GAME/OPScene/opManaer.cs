87452002a7c293f0083d4a8d9a9d1ab7dbf47b02
DESKTOP-HFP81MQ\mochi <mochi_ahphop008@yahoo.co.jp>
2017-06-04 19:55:24 +0900

    OPムービー再生の追加

---
+++ Assets/UINTY_GAME/OPScene/opManaer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class opManaer : MonoBehaviour {
+
+	VideoPlayer v;
+	// Use this for initialization
+	void Start () {
+		v = GetComponent<VideoPlayer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!v.isPlaying || Input.GetMouseButtonDown(0))
+			SceneManager.LoadScene ("Player");
+	}
+}

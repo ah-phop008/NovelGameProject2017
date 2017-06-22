03bf82824342f5c4e211955567b7daa49f617a21
ike-note\kazuki ikegami <ikenokami1998@gmail.com>
2017-06-22 19:15:30 +0900

    訂正

--- Assets/UINTY_GAME/TitleScene/gcgscript.cs
+++ Assets/UINTY_GAME/TitleScene/gcgscript.cs
@@ -4,13 +4,8 @@ using UnityEngine;
 
 public class gcgscript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+	public void OnClick() {
 		
-	}
-	
-	// Update is called once per frame
-	void Update () {
 		
-	}
+}
 }

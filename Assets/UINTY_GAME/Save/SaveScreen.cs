e3bf80bfa42253fe9f05ea25c47c7acf8a55d303
DESKTOP-HFP81MQ\mochi <mochi_ahphop008@yahoo.co.jp>
2017-07-14 19:14:36 +0900

    セーブ画面の追加。savedataの取得を制作中

---
+++ Assets/UINTY_GAME/Save/SaveScreen.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Novel;
+
+public class SaveScreen : MonoBehaviour {
+
+	public int pagenum = 0;
+	int savedataNum;
+
+	SaveData[] data = new SaveData[6];
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < 6; i++) { 
+			data [i] = new SaveData ();
+			data[i].GetSavedataText (transform.Find ("data" + (i + 1)).gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//セーブデータの情報を取得
+	void putSavedata (int pagenum) {
+		string name;
+		SaveObject obj;
+		for (int i = 0; i < 6; i++) {
+			name = "save_" + (pagenum * 6 + i + 1);
+			obj = NovelSingleton.SaveManager.getSaveData (name);
+		}
+	}
+
+
+}
+
+public class SaveData {
+	public Text num;
+	public Text chapter;
+	public Text date;
+	public Text detail;
+
+	public void GetSavedataText (GameObject obj) {
+		this.num = obj.transform.Find ("Num").gameObject.GetComponent<Text> ();
+		this.chapter = obj.transform.Find ("Chapter").gameObject.GetComponent<Text> ();
+		this.date = obj.transform.Find ("date").gameObject.GetComponent<Text> ();
+		this.detail = obj.transform.Find ("detail").gameObject.GetComponent<Text> ();
+	}
+}
---
+++ Assets/UINTY_GAME/Save/SaveUI.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SaveUI : MonoBehaviour {
+
+	private bool fadeout = false;
+	private bool bluckout = false;
+	Button[] obj;
+	GameObject savecanv;
+
+
+	public void totittle () {
+		SystemManager.SaveOption ();
+		bluckout = true;
+		startBluckout ();
+	}
+
+
+	public void back () {
+		SystemManager.SaveOption ();
+		if (SceneManager.GetActiveScene ().name == "Player") {
+			GameObject init = GameObject.Find ("InitScript");
+			init.GetComponent<SceneInit> ().messageSpeed = SystemManager.messageSpeed;
+		}
+		fadeout = true;
+		startFadeout ();
+	}
+		
+
+	void startFadeout () {
+		savecanv = GameObject.Find ("SaveCanvas");
+		obj = savecanv.GetComponentsInChildren<Button> ();
+		for (int i = 0; i < obj.Length; i++) obj [i].enabled = false;
+		savecanv.GetComponent <Fade> ().fade_out ();
+	}
+
+	void startBluckout () {
+		savecanv = GameObject.Find ("SaveCanvas");
+		obj = savecanv.GetComponentsInChildren<Button> ();
+		for (int i = 0; i < obj.Length; i++) obj [i].enabled = false;
+		savecanv.GetComponent <Fade> ().bluck_out ();
+
+	}
+
+	void Update () {
+		if (fadeout) {
+			if (savecanv.GetComponent <Fade> ().fadeCounter <= 0) {
+				//ボタンの判定を有効(元に戻す)にして、Canvasを非表示にする
+				for (int i = 0; i < obj.Length; i++)
+					obj [i].enabled = true;
+				fadeout = false;
+				savecanv.GetComponent<Canvas> ().enabled = false;
+				if (SceneManager.GetActiveScene ().name == "Player") {
+					GameObject.Find ("InitScript").GetComponent<SceneInit> ().enabled = true;
+				}
+			}
+		} else if (bluckout) {
+			//タイトルへ
+			if (savecanv.GetComponent <Fade> ().fadeCounter >= 1)
+				SceneManager.LoadScene ("title");
+		}
+	}
+}

e1b3fbe22d91c16a494c3b22ed128c572df26982
ike-note\kazuki ikegami <ikenokami1998@gmail.com>
2017-11-01 03:39:33 +0900

    kyoutuu kanryou

---
+++ Assets/JOKER_GAME/Plugins/MovieComponent.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+namespace Novel{
+	public class MovieComponent : AbstractComponent {
+
+		// public MovieComponent(){
+		// 	this.arrayVitalParam = new List<string> {
+		// 		"path", "skip"
+		// };
+		// 	this.originalParam = new Dictionary<string,string> () {
+		// 		{"path",""},
+		// 		{"skip", ""}
+		// 	};
+		// }
+
+		public MovieComponent(){
+			this.arrayVitalParam = new List<string> {
+				"path"
+		};
+			this.originalParam = new Dictionary<string,string> () {
+				{"path",""}
+			};
+		}
+
+		public override void start () {
+
+			string path = this.param["path"];
+
+			GameObject canvas = GameObject.Find("Canvas");
+
+			GameObject prefab = (GameObject)Resources.Load("Prefabs/MovieJKS");
+
+			prefab.GetComponent<RectTransform>().sizeDelta
+			 = canvas.GetComponent<RectTransform>().sizeDelta;
+
+			GameObject movieJKS = GameObject.Instantiate(prefab);
+
+			// スクリプトのメソッド呼び出し
+			MovieUI movieOnUI = movieJKS.GetComponent<MovieUI>();
+			movieOnUI.initMovie(path);
+
+			bool isSkip = true;
+			// Debug.Log(this.param["skip"]);
+			isSkip = System.Convert.ToBoolean(this.param["skip"]);
+			movieOnUI.setIsSkip(isSkip);
+			movieJKS.gameObject.name = "movie_jks";
+
+			movieJKS.transform.SetParent(canvas.transform, false);
+			
+			this.gameManager.startTag("[l]");
+		}
+	}
+}
---
+++ Assets/MovieUI.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent(typeof(RawImage))]
+[RequireComponent(typeof(AudioSource))]
+
+public class MovieUI : MonoBehaviour{
+    [SerializeField] string m_moviePath;
+
+    MovieTexture m_movieTexture = null;
+
+    RawImage m_rawImage = null;
+    AudioSource m_audioSource = null;
+    bool isSkip = false;
+
+    bool isEnd = false;
+    int count = 0;
+
+    public bool IsPlaying{
+        get { return m_movieTexture != null && m_movieTexture.isPlaying; }
+    }
+
+    public void Play(){
+        if (IsPlaying){
+            Stop();
+        }
+
+        m_movieTexture = (MovieTexture)Resources.Load<MovieTexture>(m_moviePath);
+
+        if (m_movieTexture == null){
+            Debug.LogError("movie is nothing:" + m_moviePath);
+            return;
+        }
+
+        m_movieTexture.loop = false;
+
+        m_rawImage.material.mainTexture = m_movieTexture;
+        m_audioSource.clip = m_movieTexture.audioClip;
+
+        m_movieTexture.Play();
+        m_audioSource.Play();
+    }
+
+    public void Stop(){
+        if (!IsPlaying){
+            return;
+        }
+        m_movieTexture.Stop();
+        m_audioSource.Stop();
+
+        m_movieTexture.Play();
+        m_audioSource.Play();
+
+        m_movieTexture.Stop();
+        m_audioSource.Stop();
+    }
+
+    public void setIsSkip(bool _isSkip){
+        this.isSkip = _isSkip;
+    }
+
+    public void initMovie(string _path){
+        m_moviePath = _path;
+        m_rawImage = this.GetComponent<RawImage>();
+        m_audioSource = this.GetComponent<AudioSource>();
+        Play();
+    }
+
+    void Start(){
+    }
+
+    void Update(){
+        if ((!m_movieTexture.isPlaying && m_movieTexture != null) || 
+            (this.isSkip && Input.GetMouseButtonUp(0)) ){
+            m_movieTexture.Stop();
+            m_audioSource.Stop();
+            m_movieTexture.Play();
+            m_audioSource.Play();
+            isEnd = true;
+
+            // TODO: 非表示処理が実行されない
+            GameObject movie_jks = GameObject.Find("movie_jks");
+            Renderer rend = movie_jks.GetComponent<Renderer>();
+            rend.enabled = false;
+            foreach(Transform child in movie_jks.transform){
+                GameObject obj = child.gameObject;
+                obj.GetComponent<Renderer>().enabled = false;
+            }
+        }
+
+        if(isEnd){
+            count++;
+        }
+
+        if(count > 5){
+            m_movieTexture.Stop();
+            GameObject movie_jks = GameObject.Find("movie_jks");
+            GameObject.Destroy(movie_jks);
+        }
+    }
+}
